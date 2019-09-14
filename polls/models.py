532b34304c64ddc2abd48f4ad480d871c4179afa
andi <herlandi.27@gmail.com>
2019-09-14 08:57:13 +0700

    add Choice model in admin dashboard

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# My models were Question and Choice
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
